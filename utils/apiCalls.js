3744384bb82f0810383fe52284c693b5f3473da8
Alex Kio <alexandermartelkio@gmail.com>
2021-11-28 16:23:31 -0500

    Create controlled form to post a card to API

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -1,11 +1,31 @@
-export const getData = async (url) => {
-  let response = await fetch(`${url}`)
+export const getData = async () => {
+  let response = await fetch(`https://leet-cards.herokuapp.com/cards`)
   return checkForError(response)
 }
 
+export const postData = async (postObject) => {
+  let response = await fetch(`https://leet-cards.herokuapp.com/cards`, {
+    method: 'POST',
+    body: JSON.stringify(postObject),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  return checkForError(response)
+}
+
+// export const deleteApiData = (id) => {
+//   return fetch(`https://leet-cards.herokuapp.com/cards/${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-type': 'application/json'
+//     }
+//   })
+// }
+
 const checkForError = async (response) => {
   if (!response.ok) {
-    throw new Error(response.status)
+    throw new Error(response)
   } else {
     let data = await response.json()
     return data
