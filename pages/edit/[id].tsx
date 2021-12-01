a310e5f258e3e8918cc47cebefdc5789f28980a7
Alex Kio <alexandermartelkio@gmail.com>
2021-12-01 14:49:55 -0500

    Create controlled form for updating a card's answer

--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -1,7 +1,8 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import { useState } from 'react';
-import { getData } from '../../utils/apiCalls'
+import { getData, putData } from '../../utils/apiCalls'
 import { CardType } from '../../types/index'
+import { useDeck } from "../../context/DeckContext";
 
 type Props = {
   card?: CardType
@@ -9,11 +10,20 @@ type Props = {
 }
 
 export default function EditPost({ card, errors }: Props) {
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
-  const [stack, setStack] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [category, setCategory] = useState<string>('');
+  const [answer, setAnswer] = useState(card?.answer);
+  const { updateCard } = useDeck();
+
+  const putFlashCard = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let response = await putData(card?.id, {
+      answer: answer,
+    })
+    //shore up conditional logic here: should be = if success (not error) do this
+    // if (response.id) {
+    //   addCard(response)
+    // }
+    // setAnswer('')
+  }
 
   if (errors) {
     return (
@@ -24,10 +34,20 @@ export default function EditPost({ card, errors }: Props) {
   } else if (card) {
     return (
       <>
-        {card.question}
-        {card.answer}
-        {card.side}
-        {card.categories}
+        {card.question}, {card.side}, {card.categories}
+        <form onSubmit={putFlashCard}>
+
+          <label htmlFor="answer">Answer:</label>
+          <input
+            type="text"
+            id="answer"
+            value={answer}
+            onChange={(event)=> setAnswer(event.target.value)}
+            required
+          />
+
+          <button type="submit">Submit</button>
+        </form>
       </>
     )
   }
