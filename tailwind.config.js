c9f784bf20459a35c25d2241187b5bd01c6df6c1
Alex Kio <alexandermartelkio@gmail.com>
2021-12-01 19:44:48 -0500

    Create responsive breakpoints for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'sm': '550px',
+      'md': '810px',
+      'lg': '1200px',
+      'xl': '1600px'
+    },
+    extend: {
+      fontFamily: {
+        header: ['Lora'],
+        primary: ['Epilogue']
+      }
+    },
   },
   variants: {
     extend: {},
