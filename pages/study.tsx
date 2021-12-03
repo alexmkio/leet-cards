6ef81bd8d69db76d3156a654ad1abcfd89a38988
Alex Kio <alexandermartelkio@gmail.com>
2021-12-02 19:30:12 -0500

    Add header to study page

--- pages/study.tsx
+++ pages/study.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { useDeck } from "../context/DeckContext";
 import Card from '../components/Card'
 
@@ -11,8 +12,16 @@ export default function Test() {
   })
 
   return (
-    <section className="p-4 grid grid-cols-3 place-items-center gap-8">
-      {flashCards}
-    </section>
+    <>
+      <Head>
+        <title>Leet Cards - Study the flash cards</title>
+        <meta name="description" content="Leet Cards - Study the flash cards" />
+        <meta name="theme-color" content="#F9A8D4" />
+      </Head>
+
+      <section className="p-4 grid grid-cols-3 place-items-center gap-8">
+        {flashCards}
+      </section>
+    </>
   )
 }
