0b6062e5e3c05f8f3fbf08753f0489aa113b3bc1
Alex Kio <alexandermartelkio@gmail.com>
2021-12-06 11:30:41 -0500

    Add responsivity to grid gap and padding

--- pages/study.tsx
+++ pages/study.tsx
@@ -19,7 +19,7 @@ export default function Test() {
         <meta name="theme-color" content="#F9A8D4" />
       </Head>
 
-      <section className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center gap-8">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center gap-4 lg:gap-8 p-4 lg:p-8">
         {flashCards}
       </section>
     </>
