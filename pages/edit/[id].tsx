42e422295aa253e675918d123984a271908649e6
Alex Kio <alexandermartelkio@gmail.com>
2021-12-06 11:25:24 -0500

    Add responsivity to Edit page

--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -42,16 +42,16 @@ export default function EditPost({ card }: Props) {
         <meta name="theme-color" content="#D9F99D" />
       </Head>
       
-      <h2 className="text-6xl font-semibold py-12 text-center capitalize">Edit a flash card</h2>
+      <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl pt-8 text-center capitalize">Edit a flash card</h2>
       <div className="flex flex-col items-center">
         
-        <dl className="flex flex-col text-lg">
+        <dl className="flex flex-col text-lg pt-8 md:pt-12 text-center">
           <dt className="px-4 pb-1 border-b border-gray-800">Question:</dt>
-          <dd className="pt-4 text-center">{card.question}</dd>
+          <dd className="pt-4">{card.question}</dd>
         </dl>
         
-        <form className="w-6/12 flex flex-col justify-center">
-          <label className="text-lg pt-8" htmlFor="answer">Answer:</label>
+        <form className="flex flex-col justify-center w-11/12 md:w-8/12 lg:w-6/12 xl:w-5/12">
+          <label className="text-lg pt-8 md:pt-12 text-center" htmlFor="answer">Answer:</label>
           <textarea
             className="w-full rounded-lg shadow-sm text-lg border-gray-300 focus:border-red-300 focus:ring focus:ring-red-300 focus:ring-opacity-50"
             id="answer"
@@ -61,8 +61,8 @@ export default function EditPost({ card }: Props) {
           ></textarea>
           <p className="text-red-500">{formError}</p>
 
-          <div className="flex justify-center pt-16">
-            <button onClick={(event) => putFlashCard(event)} className="flex items-center text-xl rounded-full py-3 px-9 transition duration-500 ease-in-out bg-red-300 hover:bg-red-400 transform hover:scale-110 hover:shadow-2xl hover:text-blueGray-100">
+          <div className="flex justify-center py-6 md:py-12">
+            <button onClick={(event) => putFlashCard(event)} className="flex items-center text-lg md:text-xl rounded-full py-3 px-9 transition duration-500 ease-in-out bg-red-300 hover:bg-red-400 transform hover:scale-110 hover:shadow-2xl hover:text-blueGray-100">
               Edit card&nbsp;
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
