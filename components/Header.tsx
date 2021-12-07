61126a56c155ac0e37a790555138bdfde1d4d2ad
Alex Kio <alexandermartelkio@gmail.com>
2021-12-07 18:08:27 -0500

    Correct inverting of night mode icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,17 +43,17 @@ export default function Header() {
   if (darkMode) {
     modeIcon = 
       <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
       </svg>
   } else {
     modeIcon =
       <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
       </svg>
   }
 
   return (
-    <header className="flex flex-col p-2 md:p-4 md:flex-row md:justify-between md:px-40 items-center bg-gray-50 shadow-md sticky top-0 z-50 opacity-90">
+    <header className="flex flex-col p-2 md:p-4 md:flex-row md:justify-between md:px-40 items-center bg-gray-50 dark:bg-gray-800 shadow-md sticky top-0 z-50 opacity-90">
       {logo}
       <div onClick={() => changeTheme()}>
         {modeIcon}
