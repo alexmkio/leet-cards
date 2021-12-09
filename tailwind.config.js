1bb5e0e9ed7e24cfff50e4528e7c5b3353ca4de3
Alex Kio <alexandermartelkio@gmail.com>
2021-12-09 18:02:57 -0500

    Upgrade to Tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // false or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     screens: {
       'sm': '500px',
@@ -10,39 +8,12 @@ module.exports = {
       'lg': '1400px',
       'xl': '1880px'
     },
-    colors: {
-      blueGray: colors.blueGray,
-      coolGray: colors.coolGray,
-      gray: colors.gray,
-      trueGray: colors.trueGray,
-      warmGray: colors.warmGray,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      fuchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose
-    },
     extend: {
       fontFamily: {
         header: ['Lora'],
         primary: ['Epilogue']
       }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms')
