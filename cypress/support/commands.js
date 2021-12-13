39186b80892dea12719513bcbd5d891ee8d3f77f
Alex Kio <alexandermartelkio@gmail.com>
2021-12-12 23:25:07 -0500

    Add failing edit page command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -21,3 +21,23 @@ Cypress.Commands.add('loadAddPage', () => {
     })
     .visit("http://localhost:3000/add")
 })
+
+Cypress.Commands.add('loadEditPage', () => {
+  cy.intercept("GET", `${host}cards`, {
+    statusCode: 200,
+    fixture: "cards.json",
+  })
+    .intercept("GET", `${host}cards/1`, {
+      statusCode: 200,
+      fixture: "cardNumber1.json",
+    })
+    .intercept("GET", `${host}cards/2`, {
+      statusCode: 200,
+      fixture: "cardNumber2.json",
+    })
+    .intercept("GET", `${host}cards/3`, {
+      statusCode: 200,
+      fixture: "cardNumber3.json",
+    })
+    .visit("http://localhost:3000/edit/1")
+})
