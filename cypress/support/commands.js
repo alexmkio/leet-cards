73257d626dcfab757ac80dc24482cabb817f607f
Alex Kio <alexandermartelkio@gmail.com>
2021-12-12 21:54:30 -0500

    Update Cypress command with more fixtures

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,31 @@
-// Cypress.Commands.add('loadHome', () => {
-//   cy.intercept('GET', 'https://leet-cards.herokuapp.com/cards', 
-//       { statusCode: 200, fixture: 'cards.json' })
-//     .visit('http://localhost:3000')
-// });
+const host = process.env.NEXT_PUBLIC_VERCEL_ENV
+  ? "https://leet-cards.herokuapp.com/"
+  : "http://localhost:6565/";
 
 Cypress.Commands.add('loadHome', () => {
-  cy.visit('http://localhost:3000')
-});
+  cy.intercept("GET", `${host}cards`, {
+    statusCode: 200,
+    fixture: "cards.json",
+  })
+    .intercept("GET", `${host}cards/1`, {
+      statusCode: 200,
+      fixture: "cardNumber1.json",
+    })
+    .intercept("GET", `${host}cards/2`, {
+      statusCode: 200,
+      fixture: "cardNumber2.json",
+    })
+    .intercept("GET", `${host}cards/3`, {
+      statusCode: 200,
+      fixture: "cardNumber3.json",
+    })
+    .intercept("GET", `${host}cards/4`, {
+      statusCode: 200,
+      fixture: "cardNumber4.json",
+    })
+    .intercept("POST", `${host}cards`, {
+      statusCode: 200,
+      fixture: "addedCard.json",
+    })
+    .visit("http://localhost:3000");
+})
